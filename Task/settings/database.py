c40ba13796a8d625013ea3a6dc1e3e75e613e65f
iksight <iksight@outlook.com>
2018-08-30 14:49:10 +0300

    4. Setup database.

--- Task/settings/database.py
+++ Task/settings/database.py
@@ -1,12 +1,15 @@
 import os
-from .common import BASE_DIR
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQL_DATABASE_NAME', 'task'),
+        'HOST': os.getenv('MYSQL_HOST', '127.0.0.1'),
+        'PORT': os.getenv('MYSQL_PORT', '3306'),
+        'USER': os.getenv('MYSQL_USER', 'root'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD', '9900'),
     }
 }
