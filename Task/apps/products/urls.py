44344e419e9a2cf291847b2b05aedd432058f9a3
iksight <iksight@outlook.com>
2018-08-31 16:22:11 +0300

    7. Setup adminlte2 templates. Add views for product.

--- Task/apps/products/urls.py
+++ Task/apps/products/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('products/', login_required(views.product_table), name='product_table'),
+    path('asajax/', views.product_as_json, name='product_as_json'),
 ]
--- Task/apps/products/views.py
+++ Task/apps/products/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.core import serializers
+from .models import Product
 
 # Create your views here.
 
@@ -7,3 +11,13 @@ def index(request):
     return render(request, 'index.html', {})
 
 
+@login_required
+def product_table(request):
+    products = Product.objects.all().order_by('-changed_date')
+    return render(request, 'products/product_table.html', {'products': products})
+
+
+def product_as_json(request):
+    products = Product.objects.all().order_by('-changed_date')
+    json = serializers.serialize('json', products)
+    return HttpResponse(json, content_type='application/json')
